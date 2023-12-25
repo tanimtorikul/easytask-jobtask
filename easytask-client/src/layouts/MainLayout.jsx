efe26b018f6dc2026348f66a32e24c5ed622ab43
tanimtorikul <torikulswe@gmail.com>
2023-12-25 13:38:19 +0600

    project setup completed

---
+++ easytask-client/src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/shared/Navbar/Navbar";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Outlet/>
+        </div>
+    );
+};
+
+export default MainLayout;
