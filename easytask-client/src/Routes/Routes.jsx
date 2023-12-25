e9bcaa347d685eb1be8c30d466fcdda723ad1ea8
tanimtorikul <torikulswe@gmail.com>
2023-12-25 13:51:16 +0600

    added login and signup page

--- easytask-client/src/Routes/Routes.jsx
+++ easytask-client/src/Routes/Routes.jsx
@@ -1,15 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout/>,
-      children: [{
-        path: '/',
-        element: <Home/>
-
-      }]
-    },
-  ]);
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <SignUp />,
+      },
+    ],
+  },
+]);
--- easytask-client/src/layouts/MainLayout.jsx
+++ easytask-client/src/layouts/MainLayout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/shared/Navbar/Navbar";
 
-
 const MainLayout = () => {
-    return (
-        <div>
-            <Navbar/>
-            <Outlet/>
-        </div>
-    );
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div>
+        <Navbar />
+      </div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default MainLayout;
